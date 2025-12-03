e9e7c5b15bbe12e7a7a4dd777a46f2aa34f0af43
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-12-03 14:54:54 +0900

    [Feat] PlayerManager 클래스 추가 및 Manager에 등록
    - 게임 전반적으로 Player에 대한 내용을 처리할 PlayerManager 클래스 추가.
    - 자동 생성을 위해 Manager 클래스에 등록.

--- Assets/Scripts/Manager/Manager.cs
+++ Assets/Scripts/Manager/Manager.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public static class Manager
 {
-    // public static GameManager Game => GameManager.GetInstance();
+    public static PlayerManager Game => PlayerManager.GetInstance();
 
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
     private static void Initailize()
     {
-        // GameManager.CreateInstance();
+        PlayerManager.CreateInstance();
     }
 }
