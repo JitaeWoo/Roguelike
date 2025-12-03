1d84988ddb7684c13243f0cd43057ff0826727b8
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-12-03 14:46:52 +0900

    [Feat] PlayerRuntimeData 클래스 추가
    - 게임동안 지속될 데이터가 아니라 오브젝트가 실제할 때만 다룰 데이터를 모을 PlayerRuntimeData 클래스 추가.
    - 기억해야할 정보와 그렇지 않은 정보를 나눠서 관리하기 위함.
    - 현재는 이동을 위한 변수만 추가.

---
+++ Assets/Scripts/Player/PlayerRuntimeData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRuntimeData : MonoBehaviour
+{
+    public Vector2 MoveDir;
+    public bool HasMoveInput;
+}
