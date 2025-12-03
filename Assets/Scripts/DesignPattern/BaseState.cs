21d45c2e5094a3691b917fb3fbd82452b8c6a09c
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-12-03 14:28:37 +0900

    [Feat] 상태패턴 관련 클래스 추가
    - 플레이어의 제어를 상태패턴으로 구현하기 위해 상태패턴 관련 클래스 추가.
    - 빠른 개발과 유지보수가 좋은 코드를 작성하기 위함.

---
+++ Assets/Scripts/DesignPattern/BaseState.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// T에는 각 상태를 표현할 Enum 클래스를 넣어주면 됩니다.
+public abstract class BaseState<T> where T : Enum
+{
+    protected StateMachine<T> StateMachine;
+    public bool HasPhysics;
+
+    public BaseState(StateMachine<T> stateMachine)
+    {
+        StateMachine = stateMachine;
+    }
+
+    public abstract void Enter();
+
+    public abstract void Update();
+
+    public virtual void FixedUpdate() { }
+
+    public abstract void Exit();
+}
---
+++ Assets/Scripts/DesignPattern/StateMachine.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// T에는 각 상태를 표현할 Enum 클래스를 넣어주면 됩니다.
+public class StateMachine<T> where T : Enum
+{
+    private BaseState<T> _curState;
+    public T CurStateEnum;
+    private Dictionary<T, BaseState<T>> _stateDict = new Dictionary<T, BaseState<T>>();
+
+    public void ChangeState(T changedStateEnum)
+    {
+        if (!_stateDict.ContainsKey(changedStateEnum))
+        {
+            Debug.LogError($"{changedStateEnum} 상태가 등록되지 않았습니다.");
+            return;
+        }
+
+        BaseState<T> changedState = _stateDict[changedStateEnum];
+
+        if (_curState == changedState) return;
+
+        // 처음에 없을 수도 있으니 null 체크
+        _curState?.Exit();
+        CurStateEnum = changedStateEnum;
+        _curState = changedState;
+        _curState.Enter();
+    }
+
+    public void Update() => _curState.Update();
+
+    public void FixedUpdate()
+    {
+        if (_curState.HasPhysics)
+        {
+            _curState.FixedUpdate();
+        }
+    }
+
+    public void AddState(T stateEnum, BaseState<T> state)
+    {
+        _stateDict.TryAdd(stateEnum, state);
+    }
+}
