8b2bc9dfb3cf4ad6ef8604b96532c82aa5582497
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-12-03 15:54:58 +0900

    [Feat] PlayerController에 상태머신 로직 추가
    - PlayerState에 상태머신 관련 로직 추가.
    - 임시로 만든 Idle, Move, Dash 상태를 추가하고 Idle 상태로 시작하도록 설정.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,9 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
-using R3;
 
 public class PlayerController : MonoBehaviour
 {
+    private StateMachine<PlayerStates> _stateMachine = new StateMachine<PlayerStates>();
+
+    private void Awake()
+    {
+        PlayerRuntimeData data = GetComponent<PlayerRuntimeData>();
+
+        _stateMachine.AddState(PlayerStates.Idle, new PlayerState_Idle(_stateMachine, data));
+        _stateMachine.AddState(PlayerStates.Move, new PlayerState_Move(_stateMachine, data));
+        _stateMachine.AddState(PlayerStates.Dash, new PlayerState_Dash(_stateMachine, data));
+
+        _stateMachine.ChangeState(PlayerStates.Idle);
+    }
+
+    private void Update()
+    {
+        _stateMachine.Update();
+    }
+
+    private void FixedUpdate()
+    {
+        _stateMachine.FixedUpdate();
+    }
 }
---
+++ Assets/Scripts/Player/States/PlayerState_Idle.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState_Idle : PlayerState
+{
+    public PlayerState_Idle(StateMachine<PlayerStates> stateMachine, PlayerRuntimeData data) : base(stateMachine, data)
+    {
+    }
+
+    public override void Enter()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Exit()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Update()
+    {
+        throw new System.NotImplementedException();
+    }
+}
