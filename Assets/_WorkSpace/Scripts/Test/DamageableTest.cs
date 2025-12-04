132ec96cc9ec84e87f485022c949188edeea1861
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-12-04 12:14:32 +0900

    [Test] 공격 기능 테스트
    - 공격을 테스트하기 위해 피격 대상용 DamagableTest 스크립트 추가.
    - 테스트를 위한 씬 구성.
    - 공격이 문제없이 진행됨을 확인.

---
+++ Assets/_WorkSpace/Scripts/Test/DamageableTest.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageableTest : MonoBehaviour, IDamagable
+{
+    public void TakeDamage(float amount)
+    {
+    }
+}
