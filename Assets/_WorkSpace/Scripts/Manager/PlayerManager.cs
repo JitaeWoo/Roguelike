7170d7b974f2d8ee7e12cc91a2891d4474a3eade
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-12-10 02:04:25 +0900

    [Refactor] Singleton을 Zenject로 교체
    - 앞으로 Singleton 대신 Zenject 구조를 사용하기 위하여 Manager 클래스 삭제 및 ProjectContext에서 쓸 ProjectInstaller 추가.

--- Assets/_WorkSpace/Scripts/Manager/PlayerManager.cs
+++ Assets/_WorkSpace/Scripts/Manager/PlayerManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerManager : Singleton<PlayerManager>
+public class PlayerManager
 {
     public PlayerData Data = new PlayerData();
 
