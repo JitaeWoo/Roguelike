9bfc1dc63309e66549989a014a3cf72393947569
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-12-16 16:46:54 +0900

    [Feat] 플레이어에 잔상 효과 컴포넌트 추가
    - 잔상효과를 활용할 수 있도록 Player에 잔상 효과 컴포넌트 추가 및 Zenject 의존성 추가.
    - 이후에 PlayerPresenter 스크립트로 제어할 예정.

--- Assets/_WorkSpace/Scripts/Player/PlayerInstaller.cs
+++ Assets/_WorkSpace/Scripts/Player/PlayerInstaller.cs
@@ -7,11 +7,13 @@ public class PlayerInstaller : MonoInstaller
     [SerializeField] private PlayerRuntimeData _runtimeData;
     [SerializeField] private Rigidbody2D _rigid;
     [SerializeField] private PlayerInput _input;
+    [SerializeField] private SpriteRenderer _renderer;
 
     public override void InstallBindings()
     {
         Container.Binding<PlayerRuntimeData>(_runtimeData);
         Container.Binding<Rigidbody2D>(_rigid);
         Container.Binding<PlayerInput>(_input);
+        Container.Binding<SpriteRenderer>(_renderer);
     }
 }
