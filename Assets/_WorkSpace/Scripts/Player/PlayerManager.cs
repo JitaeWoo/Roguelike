f4970e8ba83a142898397602a6319ac23d015ef7
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-12-22 12:34:59 +0900

    [Feat] PlayerManager 프리팹 추가
    - PlayerManager를 생성하기 위한 프리팹 추가.
    - ProjectContext에도 해당 수정사항 반영.

--- Assets/_WorkSpace/Scripts/Player/PlayerManager.cs
+++ Assets/_WorkSpace/Scripts/Player/PlayerManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Zenject;
 
-public class PlayerManager
+public class PlayerManager : MonoBehaviour
 {
     [SerializeField] private GameObject _playerPrefab;
 
--- Assets/_WorkSpace/Scripts/Zenject/ProjectInstaller.cs
+++ Assets/_WorkSpace/Scripts/Zenject/ProjectInstaller.cs
@@ -3,8 +3,10 @@ using Zenject;
 
 public class ProjectInstaller : MonoInstaller
 {
+    [SerializeField] private GameObject _playerManagerPrefab;
     public override void InstallBindings()
     {
         Container.Bind<PlayerManager>().AsSingle();
+        Container.Bind<PlayerManager>().FromComponentInNewPrefab(_playerManagerPrefab).AsSingle();
     }
 }
