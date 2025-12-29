51126cecb2e58266104c5279cb3a34358cdd5edc
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-12-29 15:58:15 +0900

    [Feat] MonsterData 클래스 생성
    - Monster의 각종 정보를 저장할 MonsterData 클래스 생성.
    - 간단하게 개발할 예정이므로, 일단 단순 하드 코딩.

---
+++ Assets/_WorkSpace/Scripts/Monster/MonsterData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterData : MonoBehaviour
+{
+    public float Damage = 20f;
+    public float Hp = 20f;
+    public float MoveSpeed = 5f;
+}
