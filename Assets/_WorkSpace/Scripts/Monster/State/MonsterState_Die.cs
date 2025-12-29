1080a92392f4841b09cfc63c9a7cda2832e1b8a3
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-12-29 17:31:28 +0900

    [Feat] MonsterState_Die 구현
    - 몬스터의 죽음을 처리하는 MonsterState_Die 구현.
    - 기존의 Monster에서 쓰던 로직 채용.

--- Assets/_WorkSpace/Scripts/Monster/State/MonsterState_Die.cs
+++ Assets/_WorkSpace/Scripts/Monster/State/MonsterState_Die.cs
@@ -1,9 +1,22 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Zenject;
 
 public class MonsterState_Die : MonsterState
 {
+
+    private Collider2D _collider;
+    private SpriteRenderer _renderer;
+
+    [Inject]
+    private void Init(Collider2D collider2D, SpriteRenderer renderer)
+    {
+        _collider = collider2D;
+        _renderer = renderer;
+    }
+
     public MonsterState_Die(StateMachine<MonsterStates> stateMachine) : base(stateMachine)
     {
     }
@@ -11,6 +24,12 @@ public class MonsterState_Die : MonsterState
     public override void Enter()
     {
         Data.IsDead.Value = true;
+
+        _collider.enabled = false;
+
+        _renderer.material
+            .DOFloat(0, "_SplitValue", 1f).OnComplete(() => Object.Destroy(Data.gameObject))
+            .SetLink(Data.gameObject);
     }
 
     public override void Update()
